7c8520b8ad53f35bf718459d1e02a5e30019f0cf
abulotus <abulotus@hotmail.com>
2026-06-19 18:07:37 +0100

    uph

--- backend/src/main.py
+++ backend/src/main.py
@@ -18,7 +18,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Crucial: Specify domains instead of "*" when allow_credentials=True
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
